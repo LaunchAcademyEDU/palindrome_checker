88a4c678eccc70d8531cb98e7f641fe8280080a2
Dan Pickett <dan.pickett@launchware.com>
2013-08-21 10:05:11 -0400

    extract palindrome method

--- palindrome_checker.rb
+++ palindrome_checker.rb
@@ -1,10 +1,20 @@
 puts "What is the word?"
 word = gets.chomp
 
-if word.reverse == word.reverse
+def palindrome?(a_word)
+  a_word.reverse == a_word
+end
+
+if palindrome?(word)
   puts "it's a palindrome"
 elsif word == 'bird'
   puts "the bird is the word!"
 else
   puts "it's not"
+end
+
+puts "What is another word?"
+another_word = gets.chomp
+if palindrome?(another_word)
+  puts "Another palindrome!"
 end

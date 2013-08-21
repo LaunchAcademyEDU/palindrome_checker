00274e7594496297711b323e34f921f3e1553f6b
Dan Pickett <dan.pickett@launchware.com>
2013-08-21 10:05:22 -0400

    working vowel finder

---
+++ vowel_finder.rb
@@ -0,0 +1,16 @@
+# get a word from the user
+puts "What's the word?"
+word = gets.chomp
+
+VOWELS = ['a', 'e', 'i', 'o', 'u']
+
+# output that vowel and what position it is in
+character_count = 0
+word.each_char do |char|
+  if VOWELS.include?(char)
+    puts "found the first vowel #{char} " +
+      "at position #{character_count}"
+    break
+  end
+  character_count += 1
+end

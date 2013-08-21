61dca9027ff33201026595b18c0fb0909f7f3d49
Dan Pickett <dan.pickett@launchware.com>
2013-08-21 10:16:11 -0400

    extracted vowel method

--- vowel_finder.rb
+++ vowel_finder.rb
@@ -4,10 +4,15 @@ word = gets.chomp
 
 VOWELS = ['a', 'e', 'i', 'o', 'u']
 
+def vowel?(char_to_check, set_of_vowels = VOWELS)
+  set_of_vowels.include?(char_to_check)
+end
+
 # output that vowel and what position it is in
 character_count = 0
 word.each_char do |char|
-  if VOWELS.include?(char)
+  puts "#{char}: #{character_count}"
+  if vowel?(char)
     puts "found the first vowel #{char} " +
       "at position #{character_count}"
     break
